8ce7e5e1af8b573b4ee6bf822d2fb0a92b47c993
facuuu11 <seconecto.elchipa@gmail.com>
2025-05-27 12:02:13 -0300

    Mejoras de codigo
    Agregamos limite del juego y correccion en la caida de los barriles (parcial)

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 import random
 
-
 pygame.init()
 
 
 WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Donkey Kong Simple")
+clock = pygame.time.Clock()
 FPS = 60
 
 
@@ -13,11 +15,7 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 BROWN = (139, 69, 19)
 RED = (255, 0, 0)
-
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Donkey Kong Simple")
-clock = pygame.time.Clock()
+BARREL_COLOR = (128, 0, 0)
 
 
 player = pygame.Rect(100, HEIGHT - 60, 30, 40)
@@ -34,18 +32,47 @@ platforms = [
 ]
 
 
-barrels = []
-
-
 princess = pygame.Rect(700, 100, 30, 40)
 
 
-def spawn_barrel():
-    barrels.append(pygame.Rect(50, 140, 20, 20))
+class Barrel:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, 20, 20)
+        self.direction = 1 
+        self.falling = False
+        self.vel_y = 0
+
+    def update(self):
+        if self.falling:
+            self.vel_y += GRAVITY
+            self.rect.y += self.vel_y
+            
+            for plat in platforms:
+                if self.rect.colliderect(plat) and self.vel_y >= 0:
+                    self.rect.bottom = plat.top
+                    self.falling = False
+                    self.vel_y = 0
+                    self.direction *= -1  
+        else:
+            self.rect.x += 2 * self.direction
+            
+            under_platform = None
+            for plat in platforms:
+                if plat.collidepoint(self.rect.midbottom):
+                    under_platform = plat
+                    break
+            if not under_platform:
+                self.falling = True
+
+    def draw(self, screen):
+        pygame.draw.ellipse(screen, BARREL_COLOR, self.rect)
 
 
-barrel_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(barrel_timer, 2000)
+barrels = []
+
+
+SPAWN_BARREL = pygame.USEREVENT + 1
+pygame.time.set_timer(SPAWN_BARREL, 2000)
 
 
 running = True
@@ -53,12 +80,12 @@ while running:
     clock.tick(FPS)
     screen.fill(WHITE)
 
-
+   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == barrel_timer:
-            spawn_barrel()
+        if event.type == SPAWN_BARREL:
+            barrels.append(Barrel(50, 130))
 
     
     keys = pygame.key.get_pressed()
@@ -66,43 +93,48 @@ while running:
         player.x -= 5
     if keys[pygame.K_RIGHT]:
         player.x += 5
+
+    
+    player.x = max(0, min(WIDTH - player.width, player.x))
+
+    
     if keys[pygame.K_SPACE] and not is_jumping:
-        is_jumping = True
         player_vel_y = -15
+        is_jumping = True
 
     
     player_vel_y += GRAVITY
     player.y += player_vel_y
 
- 
+    
     for plat in platforms:
         if player.colliderect(plat) and player_vel_y >= 0:
             player.bottom = plat.top
             is_jumping = False
             player_vel_y = 0
 
-
+    
     for plat in platforms:
         pygame.draw.rect(screen, BROWN, plat)
 
- 
-    pygame.draw.rect(screen, BLUE, player)
-
- 
+    
     pygame.draw.rect(screen, RED, princess)
 
     
-    for barrel in barrels[:]:
-        barrel.x += 3
-        if barrel.x > WIDTH:
-            barrels.remove(barrel)
-        pygame.draw.ellipse(screen, (128, 0, 0), barrel)
+    pygame.draw.rect(screen, BLUE, player)
 
-        if player.colliderect(barrel):
+    
+    for barrel in barrels[:]:
+        barrel.update()
+        barrel.draw(screen)
+
+        if barrel.rect.top > HEIGHT:
+            barrels.remove(barrel)
+
+        if player.colliderect(barrel.rect):
             print("¡Perdiste!")
             running = False
 
-    
     if player.colliderect(princess):
         print("¡Ganaste!")
         running = False
